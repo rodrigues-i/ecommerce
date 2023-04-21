291b93b5b0a46bee894d1d4e1e38ed272d587f52
rodrigues-i <lucas-f250@live.com>
2023-04-21 12:51:43 -0300

    Adiciona lombok ao projeto e cria a entidade Customer

---
+++ backend/src/main/java/com/rodrigues/ecommerce/entity/Customer.java
@@ -0,0 +1,25 @@
+package com.rodrigues.ecommerce.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long customerId;
+	private String firstName;
+	private String familyName;
+	private String email;
+	private String password;
+}
