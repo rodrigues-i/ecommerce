70ec8ca6b51203dca24e8103ef4ebd4841fecedd
rodrigues-i <lucas-f250@live.com>
2023-04-21 13:08:38 -0300

    Corrigi annotation

--- backend/src/main/java/com/rodrigues/ecommerce/customer/CustomerController.java
+++ backend/src/main/java/com/rodrigues/ecommerce/customer/CustomerController.java
@@ -3,8 +3,9 @@ package com.rodrigues.ecommerce.customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,8 +19,8 @@ public class CustomerController {
 	@Autowired
 	private CustomerService customerService;
 
-	@GetMapping
-	public ResponseEntity<Long> createCustomer(@PathVariable Customer customer) {
+	@PostMapping
+	public ResponseEntity<Long> createCustomer(@RequestBody Customer customer) {
 		Long id = customerService.createCustomer(customer);
 		return ResponseEntity.status(HttpStatus.CREATED).body(id);
 	}
