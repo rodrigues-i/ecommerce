1dd77370c920fd467874a06af489ba469df09ae0
rodrigues-i <lucas-f250@live.com>
2023-04-21 21:21:46 -0300

    Cria middleware para lidar com exceções no sistema

--- backend/src/main/java/com/rodrigues/ecommerce/controller/exceptions/StandardError.java
+++ backend/src/main/java/com/rodrigues/ecommerce/controller/exceptions/StandardError.java
@@ -10,11 +10,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class StandardError implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	private Instant timestamp;
 	private Integer status;
 	private String error;
 	private String message;
 	private String path;
-	
+
 }
