6be091035071ff24c0d0ed9c58017c00525e9c84
rodrigues-i <lucas-f250@live.com>
2023-04-22 18:55:45 -0300

    Cria testes unitários para método do customerService que busca customer por id

--- backend/src/test/java/com/rodrigues/ecommerce/service/CustomerServiceTest.java
+++ backend/src/test/java/com/rodrigues/ecommerce/service/CustomerServiceTest.java
@@ -1,6 +1,11 @@
 package com.rodrigues.ecommerce.service;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.verify;
+import static org.mockito.BDDMockito.given;
+
+import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,7 +13,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.rodrigues.ecommerce.entity.Customer;
 import com.rodrigues.ecommerce.repository.CustomerRepository;
+import com.rodrigues.ecommerce.service.exceptions.ResourceNotFoundException;
 
 @ExtendWith(MockitoExtension.class)
 public class CustomerServiceTest {
@@ -29,6 +36,37 @@ public class CustomerServiceTest {
 
 		// then
 		verify(customerRepository).findAll();
+	}
+	
+	@Test
+	void canGetCustomerById() {
+		// given
+		Long customerId = 1L;
+		Customer customer = new Customer(customerId, "Pedro", "Pereira", "pedro@email.com", "senha");
+		Optional<Customer> obj = Optional.of(customer);
+		
+		given(customerRepository.findById(customerId))
+			.willReturn(obj);
+		
+		// when
+		underTest.getCustomerById(customerId);
+		
+		// then
+		verify(customerRepository).findById(customerId);
+	}
+	
+	@Test
+	void canThrowResourceNotFoundExceptionWhenCustomerDoesNotExist() {
+		// given
+		Long customerId = 1L;
+		given(customerRepository.findById(customerId))
+			.willReturn(Optional.empty());
+		
+		// when		
+		// then
+		assertThatThrownBy(() -> underTest.getCustomerById(customerId))
+			.isInstanceOf(ResourceNotFoundException.class)
+			.hasMessageContaining("Customer not found for id " + customerId);
 	}
 
 }
