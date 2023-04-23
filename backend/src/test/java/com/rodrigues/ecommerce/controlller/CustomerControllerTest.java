df89d1128acccb10fb72006676bd352aba333e19
rodrigues-i <lucas-f250@live.com>
2023-04-23 17:52:27 -0300

    Cria teste unitário para métodos do controller que buscam usuário

---
+++ backend/src/test/java/com/rodrigues/ecommerce/controlller/CustomerControllerTest.java
@@ -0,0 +1,84 @@
+package com.rodrigues.ecommerce.controlller;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.rodrigues.ecommerce.controller.CustomerController;
+import com.rodrigues.ecommerce.entity.Customer;
+import com.rodrigues.ecommerce.service.CustomerService;
+import com.rodrigues.ecommerce.service.exceptions.ResourceNotFoundException;
+
+@WebMvcTest(CustomerController.class)
+public class CustomerControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+	@MockBean
+	private CustomerService customerService;
+
+	@BeforeEach
+	void setUp() {
+
+	}
+
+	@Test
+	void getAllCustomersShouldReturnCustomers() throws Exception {
+		Customer customer1 = new Customer(1L, "Nome1", "Sobrenome1", "meu@email.com", "pass");
+		Customer customer2 = new Customer(2L, "Nome2", "Sobrenome2", "o.email@email.com", "senha");
+		String jsonString = "[{\"customerId\":1,\"firstName\":\"Nome1\",\"familyName\":\"Sobrenome1\",\"email\":\"meu@email.com\",\"password\":\"pass\"},{\"customerId\":2,\"firstName\":\"Nome2\",\"familyName\":\"Sobrenome2\",\"email\":\"o.email@email.com\",\"password\":\"senha\"}]";
+
+		List<Customer> customers = Arrays.asList(customer1, customer2);
+		when(customerService.getAllCustomers()).thenReturn(customers);
+
+		mockMvc.perform(get("/customers").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+				.andExpect(content().json(jsonString))
+				.andExpect(jsonPath("$[0].customerId").value(customer1.getCustomerId()))
+				.andExpect(jsonPath("$[0].firstName").value(customer1.getFirstName()))
+				.andExpect(jsonPath("$[0].familyName").value(customer1.getFamilyName()))
+				.andExpect(jsonPath("$[1].customerId").value(customer2.getCustomerId()))
+				.andExpect(jsonPath("$[1].firstName").value(customer2.getFirstName()))
+				.andExpect(jsonPath("$[1].familyName").value(customer2.getFamilyName()));
+
+		verify(customerService).getAllCustomers();
+	}
+
+	@Test
+	void getCustomerByIdShouldReturnCustomer() throws Exception {
+		Customer customer = new Customer(1L, "John", "Doe", "johndoe@example.com", "pass");
+		when(customerService.getCustomerById(1L)).thenReturn(customer);
+
+		mockMvc.perform(get("/customers/1").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+				.andExpect(jsonPath("$.customerId").value(customer.getCustomerId()))
+				.andExpect(jsonPath("$.firstName").value(customer.getFirstName()))
+				.andExpect(jsonPath("$.familyName").value(customer.getFamilyName()))
+				.andExpect(jsonPath("$.email").value(customer.getEmail()));
+
+		verify(customerService).getCustomerById(customer.getCustomerId());
+	}
+
+	@Test
+	void whenGetCustomerByIdWithInvalidIdThenThrowsResourceNotFoundException() throws Exception {
+		Long customerId = 1L;
+		when(customerService.getCustomerById(customerId)).thenThrow(ResourceNotFoundException.class);
+
+		mockMvc.perform(get("/customers/{id}", customerId)).andExpect(status().isNotFound());
+
+		verify(customerService).getCustomerById(customerId);
+	}
+}
