5603a280209683d91309f1da5bc953b7f258b791
rodrigues-i <lucas-f250@live.com>
2023-04-26 19:19:09 -0300

    Cria teste unitário para método do service que atualiza seller

--- backend/src/test/java/com/rodrigues/ecommerce/service/SellerServiceTest.java
+++ backend/src/test/java/com/rodrigues/ecommerce/service/SellerServiceTest.java
@@ -3,6 +3,7 @@ package com.rodrigues.ecommerce.service;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
 import java.util.Optional;
@@ -71,5 +72,52 @@ public class SellerServiceTest {
 		// then
 		assertThat(id).isEqualTo(seller.getSellerId());
 		verify(sellerRepository).save(seller);
+	}
+	
+	@Test
+	public void updateSellerShouldReturnUpdatedSeller() {
+		// given
+		String newName = "newName";
+		seller.setFirstName(newName);
+		
+		given(sellerRepository.save(seller)).willReturn(seller);
+		given(sellerRepository.findById(sellerId)).willReturn(optional);
+		
+		// when
+		Seller updatedSeller = underTest.updateSeller(sellerId, seller);
+		
+		// then
+		assertThat(newName).isEqualTo(updatedSeller.getFirstName());
+		verify(sellerRepository).findById(sellerId);
+		verify(sellerRepository).save(seller);
+	}
+	
+	@Test
+	public void updateSellerShouldReturnNullWhenSellerDoesNotExist() {
+		// given
+		Long nonExistingId = 3L;
+		
+		// when
+		Seller result = underTest.updateSeller(nonExistingId, seller);
+		
+		// then
+		assertThat(result).isNull();
+		verify(sellerRepository, never()).findById(nonExistingId);
+		verify(sellerRepository, never()).save(seller);
+	}
+	
+	@Test
+	public void updateSellerShouldThrowResourceNotFoundExceptionWhenSellerDoesNotExist() {
+		// given
+		given(sellerRepository.findById(sellerId)).willReturn(Optional.empty());
+		
+		// when
+		// then
+		assertThatThrownBy(() -> underTest.updateSeller(sellerId, seller))
+			.isInstanceOf(ResourceNotFoundException.class)
+			.hasMessageContaining("Seller not found for id " + sellerId);
+		
+		verify(sellerRepository).findById(sellerId);
+		verify(sellerRepository, never()).save(seller);
 	}
 }
