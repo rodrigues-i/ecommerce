e560d47a67a3234d2e36f2496e31d6153ebc8298
rodrigues-i <lucas-f250@live.com>
2023-04-28 19:20:41 -0300

    Cria entidade product

---
+++ backend/src/main/java/com/rodrigues/ecommerce/entity/Product.java
@@ -0,0 +1,20 @@
+package com.rodrigues.ecommerce.entity;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+
+	@NotBlank(message = "Name is required")
+	private String name;
+	@NotBlank(message = "Description is required")
+	private String description;
+}
