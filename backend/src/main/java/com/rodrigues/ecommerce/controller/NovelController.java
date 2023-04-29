aed03e3d68d095398d6efd564f6cfd9ad5a9ec5a
rodrigues-i <lucas-f250@live.com>
2023-04-29 20:46:33 -0300

    Cria NovelController e métodos para buscar e criar novel

---
+++ backend/src/main/java/com/rodrigues/ecommerce/controller/NovelController.java
@@ -0,0 +1,38 @@
+package com.rodrigues.ecommerce.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rodrigues.ecommerce.entity.Novel;
+import com.rodrigues.ecommerce.service.NovelService;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@RequestMapping(value = "/novels")
+@AllArgsConstructor
+public class NovelController {
+
+	private final NovelService novelService;
+
+	@GetMapping
+	public ResponseEntity<List<Novel>> getAllNovels() {
+		List<Novel> novels = novelService.getAllNovels();
+		return ResponseEntity.ok().body(novels);
+	}
+
+	@PostMapping
+	public ResponseEntity<Long> createNovel(@Valid @RequestBody Novel novel) {
+		Long createdNovelId = novelService.createNovel(novel);
+		return ResponseEntity.status(HttpStatus.CREATED).body(createdNovelId);
+	}
+}
