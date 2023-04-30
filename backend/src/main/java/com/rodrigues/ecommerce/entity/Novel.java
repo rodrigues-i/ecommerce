eefa9c0d37baf4477a69ef3ece706decbb88f3f4
rodrigues-i <lucas-f250@live.com>
2023-04-29 21:08:09 -0300

    Adiciona annotations faltando na entidade Novel

--- backend/src/main/java/com/rodrigues/ecommerce/entity/Novel.java
+++ backend/src/main/java/com/rodrigues/ecommerce/entity/Novel.java
@@ -1,6 +1,10 @@
 package com.rodrigues.ecommerce.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.validation.constraints.NotBlank;
 
 import lombok.AllArgsConstructor;
@@ -13,9 +17,12 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Novel {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long novelId;
 	@NotBlank(message = "Name is required")
 	private String name;
 	@NotBlank(message = "Description is required")
+	@Column(columnDefinition = "TEXT", length = 255)
 	private String description;
 }
